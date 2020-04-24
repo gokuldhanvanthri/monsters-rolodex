feecbd71f5e71abe668bdcb8a54befe85569d9af
Gokulkrishna Dhanvanthri <gokuld@asiapac.altair.com>
2020-04-24 17:32:54 +0530

    Updates

---
+++ precache-manifest.020cdabea6256fc3cee830eb26605cae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1108c8ffe4c3405a71a519768201fe62",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b672f79b9eb1d555fa2c",
+    "url": "/monsters-rolodex/static/css/main.2c018ca2.chunk.css"
+  },
+  {
+    "revision": "8aef2538accc0c8ef9b8",
+    "url": "/monsters-rolodex/static/js/2.cb54538f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.cb54538f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b672f79b9eb1d555fa2c",
+    "url": "/monsters-rolodex/static/js/main.632c710d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
